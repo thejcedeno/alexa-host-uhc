b301a5076c91f6e7b9d4db71416767f419ca7760
thejcedeno <jcedeno@panicsoda.com>
2023-03-25 02:58:42 -0400

    feat: it somewhat works lol

--- src/python/script.py
+++ src/python/script.py
@@ -6,16 +6,19 @@ import subprocess
 
 from enum import Enum
 
+
 class InstanceType(Enum):
     SHARED = 'shared'
     DEDICATED = 'dedicated'
     COMPUTE_OPTIMIZED = 'compute_optimized'
     HIGH_FREQUENCY = 'high_frequency'
     BAREMETAL = 'baremetal'
-    
+
+
 class TerrainType(Enum):
     VANILLA = 'vanilla'
     RUN = 'run'
+
 
 @dataclass
 class ResourceAllocationRequest:
@@ -28,25 +31,38 @@ class ResourceAllocationRequest:
     cpus: int
     # The type of instance to allocate
     instance_type: InstanceType
-    
+
+
 @dataclass
 class GameConfig:
     """ A class to hold UHC match game config. """
     world_seed: str
     terrain_generation: TerrainType
-    game_host: str # ideally a UUID
+    game_host: str  # ideally a UUID
     scenarios: List[str]
     nether: bool
     end: bool
-    
+
+
+@dataclass
+class Requestor:
+    """ Represents the requestor of a server creation request. """
+    # A uniqu id for the requestor
+    id: str
+    # The name of the requestor
+    name: str
+    # The email of the requestor
+    email: str
+
 
 @dataclass
 class ServerCreationRequest:
     # The name of the docker image to be used in the deployment
     container_image: str
     resources: ResourceAllocationRequest
-    
-    
+    requestor: Requestor
+
+
 @dataclass
 class ServerCreationResponse:
     """ Represents the response to a server creation request. """
@@ -60,24 +76,43 @@ class ServerCreationResponse:
     port: int
     # The game config of the server
     game_config: GameConfig
-    
+
 
 # Post request
 def create_infraestructure(req: ServerCreationRequest) -> ServerCreationResponse:
     """ Deploys new infraestructure from provided template."""
     # declare variables
     vm_name = generate_unique_match_game(req)
-    
-    
+    region = get_region(req)
+    plan = get_optimal_plan(req)
+
     # Run a bash command to terraform deploy
-    terraform_cmd = ['terraform', 'apply', '-var', 'vm_name=my-vm', '-var', 'region=nrt', '-var', 'plan=vc2-1c-1gb', '-var', 'os_id=387']
+    terraform_cmd = ['terraform', 'apply', '-var',
+                     f'vm_name={vm_name}', '-var', f'region={region}', '-var', f'plan={plan}', '-var', 'os_id=387']
+    
+    print(f'Running command: {terraform_cmd}')
 
     # Run the Terraform command
     result = subprocess.run(terraform_cmd, capture_output=True)
 
     # Print the output of the Terraform command
     print(result.stdout.decode())
-    
+
+
 def generate_unique_match_game(user_creation_request: ServerCreationRequest) -> str:
     """ Generates a unique match game name. """
     return '1234-1234-1234-1234'
+
+
+def get_region(req: ServerCreationRequest) -> str:
+    """ Gets the region of the server. """
+    return 'nrt'
+
+
+def get_optimal_plan(req: ServerCreationRequest) -> str:
+    """ Gets the optimal plan for the server. """
+    return 'vc2-1c-1gb'
+
+
+create_infraestructure(ServerCreationRequest("doesn't matter now lol xd.", ResourceAllocationRequest('e085d739-25e3-4990-a2ec-98e84f5e03d9',
+                       1024, 1, InstanceType.SHARED), Requestor('5de6e184-af8d-498a-bbde-055e50653316', 'jcedeno', 'jcedeno@jcedeno.us')))
